6d1f5abfa0d277001cdc3f6bc64fbc2c9e7e9441
nsjames <skyandbeyond@gmail.com>
2019-09-01 16:28:11 +0300

    signers

--- packages/core/lib/services/secure/index.js
+++ packages/core/lib/services/secure/index.js
@@ -2,10 +2,12 @@ import HardwareService from './HardwareService';
 import KeyPairService from './KeyPairService';
 import QRService from './QRService';
 import Seeder from './Seeder';
+import SigningService from './SigningService';
 
 export default {
 	HardwareService,
 	KeyPairService,
 	QRService,
 	Seeder,
+	SigningService,
 }
