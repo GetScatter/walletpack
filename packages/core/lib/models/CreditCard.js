1e17ccd5cfdcdcf59b3c2e26ea6c8e2040d9407e
nsjames <skyandbeyond@gmail.com>
2019-11-28 01:27:42 +0200

    expiration for cc shouldnt be in secure props

--- packages/core/lib/models/CreditCard.js
+++ packages/core/lib/models/CreditCard.js
@@ -26,7 +26,6 @@ export class CreditCardSecureProperties {
     constructor(){
         this.number = '';
         this.authTokens = {};
-	    this.expiration = '';
 	    this.cardHash = '';
 
         this.personalInformation = CreditCardPersonalInformation.placeholder();
@@ -49,6 +48,7 @@ export default class CreditCard {
         this.id = IdGenerator.text(24);
         this.name = '';
 	    this.lastFour = '';
+	    this.expiration = '';
         this.secure = CreditCardSecureProperties.placeholder();
         this.createdAt = +new Date();
     }
