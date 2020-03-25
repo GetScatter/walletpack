5d86435bfa664b1fd84015020a4de9810f8cfa68
nsjames <skyandbeyond@gmail.com>
2020-03-25 23:02:32 +0200

    prepares

--- packages/core/prepare.js
+++ packages/core/prepare.js
@@ -7,11 +7,15 @@ const parent = paths[paths.length-2];
 
 if(parent === 'packages') return;
 
-rimraf.sync("./__tests__");
-const files = fs.readdirSync(`./dist`);
-files.map(file => {
-	if(fs.existsSync(`./${file}`)) rimraf.sync(`./${file}`);
-	fs.renameSync(`./dist/${file}`, `./${file}`);
-})
-rimraf.sync("./dist");
-rimraf.sync("./lib");
+try {
+	rimraf.sync("./__tests__");
+	const files = fs.readdirSync(`./dist`);
+	files.map(file => {
+		if(fs.existsSync(`./${file}`)) rimraf.sync(`./${file}`);
+		fs.renameSync(`./dist/${file}`, `./${file}`);
+	})
+	rimraf.sync("./dist");
+	rimraf.sync("./lib");
+} catch(e){
+	console.error('Walletpack prepare.js error', e);
+}
