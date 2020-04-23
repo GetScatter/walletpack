a7c35452a2d7a7d5659c50e8345005cba33e5941
nsjames <skyandbeyond@gmail.com>
2020-04-23 14:09:26 +0300

    nullfix

--- packages/core/lib/services/apis/ApiService.js
+++ packages/core/lib/services/apis/ApiService.js
@@ -417,8 +417,11 @@ export default class ApiService {
 
 	static async [Actions.CREATE_ENCRYPTION_KEY](request){
 		let {origin, scatterPublicKey, otherPublicKey, nonce} = request.payload;
-		if(nonce) nonce = nonce.toString().trim();
-		if(!nonce.length) nonce = null;
+		if(nonce) {
+			nonce = nonce.toString().trim();
+			if(!nonce.length) nonce = null;
+		}
+
 
 		const identity = PermissionService.identityFromPermissions(origin, false);
 		if(!identity) return {id:request.id, result:Error.identityMissing()};
